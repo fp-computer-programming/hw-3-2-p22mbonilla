08d9a1c8debc371265139c891b83611212ca0a03
p22mbonilla <p22mbonilla@fairfieldprep.org>
2021-10-06 10:07:19 -0400

    complete

--- hw3-2-2.py
+++ hw3-2-2.py
@@ -1,7 +1,14 @@
 # Author MB 10/04/2021
 
-w = int(input("what is your weight"))
+w = int(input("what is your weight: "))
 
 if w >= 40:
     print("you are Extremely Obese")
-elif
+elif w >= 30:
+    print("you are Obese")
+elif w >= 25:
+    print("you are Overweight")
+elif w >= 19:
+    print("you are Healthy")
+else:
+    print("you are Underweight")
